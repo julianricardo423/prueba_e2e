ae50d201620526e44c57b9bf806882bf92b835cb
julianricardo423 <julianricardo423@gmail.com>
2026-01-25 16:09:12 -0500

    Archivos modificados

--- target/test-results/timeline/report.js
+++ target/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"d1ded0a3-290f-4c04-b565-1bda9cae537b","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769368190481,"group":3,"content":"","tags":"@cucumber,@adding,","end":1769368230695,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":3,"content":"Thread[#3,main,5,main]"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"01cbfdc2-49a7-4ab8-b2f6-fc755418252f","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769375062917,"group":1,"content":"","tags":"@cucumber,@adding,","end":1769375098208,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
 });
