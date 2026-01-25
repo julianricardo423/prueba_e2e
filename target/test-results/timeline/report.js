ec8c907cfd64d52ac233ebaafec7e1dfab980412
julianricardo423 <julianricardo423@gmail.com>
2026-01-25 12:29:10 -0500

    cambio en el pom.xml

--- target/test-results/timeline/report.js
+++ target/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"f2d00da9-a0cc-4b43-8c0a-2daf0e31463b","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769271883290,"group":1,"content":"","tags":"@cucumber,@adding,","end":1769271921696,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"3a48c30e-aa4d-4144-9b18-c441d7ff2a6a","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769360707425,"group":1,"content":"","tags":"@cucumber,@adding,","end":1769360735202,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
 });
