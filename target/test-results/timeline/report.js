6268b57e5d76c9008e21bff508b84dffd4f49431
julianricardo423 <julianricardo423@gmail.com>
2026-01-25 14:33:40 -0500

    Archivos modificados serenity.conf

--- target/test-results/timeline/report.js
+++ target/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"1d06cc55-9572-46a0-a653-c012c901c673","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769367835764,"group":1,"content":"","tags":"@cucumber,@adding,","end":1769367881211,"className":"passed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[#1,main,5,main]"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"d1ded0a3-290f-4c04-b565-1bda9cae537b","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769368190481,"group":3,"content":"","tags":"@cucumber,@adding,","end":1769368230695,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":3,"content":"Thread[#3,main,5,main]"}]);
 });
