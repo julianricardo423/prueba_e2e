12a97b140b2ed57beb00256697bad686c12175f4
julianricardo423 <julianricardo423@gmail.com>
2026-01-25 21:29:43 -0500

    gitignore

--- target/test-results/timeline/report.js
+++ target/test-results/timeline/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"09665ecd-ef3b-4a27-84a0-f3ba4090da07","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769387817785,"group":3,"content":"","tags":"@cucumber,@adding,","end":1769387862198,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"1426ec68-8c0c-44d9-bea5-68b451172c0a","feature":"Automation Exercise - Contact Us","scenario":"Enviar mensaje en opción contact us y volver al home page","start":1769392685373,"group":3,"content":"","tags":"@cucumber,@adding,","end":1769392722903,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":3,"content":"Thread[#3,main,5,main]"}]);
 });
